0dcfd1682485e0b8a06b60cc482124a9f8b0adf2
Oleg Shpadin <73585953+shpadio@users.noreply.github.com>
2023-04-03 00:58:11 +0500

    server runs with nodemon, socket instance created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import * as http from "http";
 import * as process from "process";
+import { Server } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 8090;
+
+export const io = new Server(server,{
+
+})
+
+io.on('connection', (socket) => {
+    console.log(`${socket} is connected`)
+})
 
 server.listen(port, () => `Server is running on ${port}`)
